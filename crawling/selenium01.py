62db0c07ff93209ae293ed1dade03e3f2a5841d8
jayhyeon12 <jhkk0124@icloud.com>
2024-04-16 16:21:05 +0900

    crawling

--- crawling/selenium01.py
+++ crawling/selenium01.py
@@ -7,7 +7,7 @@ driver = webdriver.Chrome()
 
 driver.get("https://comic.naver.com/index")
 
-time.sleep(2)
+time.sleep(1)
 
 webtoonMenu = driver.find_element(by=By.XPATH, value='//*[@id="menu"]/li[2]/a')
 webtoonMenu.click()
@@ -16,7 +16,13 @@ time.sleep(1)
 
 
 webtoonDict = {
-    "월": []  
+    "월": [],  
+    "화": [],  
+    "수": [],  
+    "목": [],  
+    "금": [],  
+    "토": [],  
+    "일": []
 }
 
 dayList = list(webtoonDict.keys())
@@ -41,12 +47,12 @@ for day in dayList:
                 break
 
         webtoonList = []
-        webtoonItems = driver.find_elements(by=By.CSS_SELECTOR, value='ContentList__content_list--q5KXY .item')
+        webtoonItems = driver.find_elements(by=By.CSS_SELECTOR, value='.ContentList__content_list--q5KXY .item')
         for item in webtoonItems:
-            title = item.find_element(by=By.CSS_SELECTOR, value='ContentTitle__title--e3qXt .text').text
-            author = item.find_element(by=By.CSS_SELECTOR, value='ContentAuthor__author--CTAAP').text
-            rating = item.find_element(by=By.CSS_SELECTOR, value='Rating__star_area--dFzsb .text').text
-            imgUrl = item.find_element(by=By.CSS_SELECTOR, value='Poster__image--d9XTI').get_attribute(name="src")
+            title = item.find_element(by=By.CSS_SELECTOR, value='.ContentTitle__title--e3qXt .text').text
+            author = item.find_element(by=By.CSS_SELECTOR, value='.ContentAuthor__author--CTAAP').text
+            rating = item.find_element(by=By.CSS_SELECTOR, value='.Rating__star_area--dFzsb .text').text
+            imgUrl = item.find_element(by=By.CSS_SELECTOR, value='.Poster__image--d9XTI').get_attribute(name="src")
 
             webtoon = {
                 "title": title,
@@ -57,13 +63,13 @@ for day in dayList:
 
             webtoonList.append(webtoon)
         
-        webtoonDict[day] = webtoonList # webtoonList.update({day: webtoonList})
+        webtoonDict.update({day: webtoonList})
 
 host = ""
 port = 3306
 user = ""
 password = ""
-database = ""
+database = "webtoon_db"
 
 connection = pymysql.connect(host=host, port=port, user=user, password=password, database=database)
 cursor = connection.cursor()
